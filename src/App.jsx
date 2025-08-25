b93645d75ec38f0c791836112491f36aa0fa829e
Hirun4 <hirun08072002@gmail.com>
2025-08-25 10:19:46 +0530

    Refactor routing and add 404 page for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Welcome from './Pages/Welcome'
 import UserDashboard from './Pages/User/Dashboard'
 import AdminDashboard from './Pages/Admin/Dashboard'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/restaurant-pos/' element={<Welcome />} />
-        <Route path='/restaurant-pos/user/dashboard' element={<UserDashboard />} />
-        <Route path='/restaurant-pos/admin/dashboard' element={<AdminDashboard />} />
+        <Route path='/' element={<Welcome />} />
+        <Route path='/user/dashboard' element={<UserDashboard />} />
+        <Route path='/admin/dashboard' element={<AdminDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
