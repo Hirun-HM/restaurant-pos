2947b63aa758785af82ed36fbb5700d33c9c1482
Hirun4 <hirun08072002@gmail.com>
2025-08-25 10:23:16 +0530

    Fix GitHub Pages routing - use BrowserRouter with proper 404.html redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Welcome from './Pages/Welcome'
 import UserDashboard from './Pages/User/Dashboard'
 import AdminDashboard from './Pages/Admin/Dashboard'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/restaurant-pos">
       <Routes>
         <Route path='/' element={<Welcome />} />
         <Route path='/user/dashboard' element={<UserDashboard />} />
         <Route path='/admin/dashboard' element={<AdminDashboard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
