3d276ddab8f03a7881f7d4e25167534a68786da8
Hirun4 <hirun08072002@gmail.com>
2025-09-15 06:07:28 +0530

    Fix routing base path in App component and update Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    base: './',
+    base: '/restaurant-pos/',
     plugins: [react()],
     server: {
       port: 5000,
